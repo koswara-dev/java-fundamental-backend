b3ac2464f992293612cfeea0475e90fd8cbc1827
Koswara <koswara.c79@gmail.com>
2025-02-06 21:38:21 +0700

    method java full

--- src/main/java/com/juaracoding/MethodJava.java
+++ src/main/java/com/juaracoding/MethodJava.java
@@ -1,5 +1,7 @@
 package com.juaracoding;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 public class MethodJava {
@@ -13,6 +15,21 @@ public class MethodJava {
         drawKotak(10);
 
         System.out.println(luasKotak(7));
+
+        System.out.println(isLogin("admin","p4ssw0rd"));
+
+        displayDetailProduct("Thinkplus T50",125000,352,100);
+
+        double[] prices = {29.99,9.99,11.77};
+        double totalPrice = totalPrice(prices);
+        System.out.println(totalPrice);
+        System.out.println(totalPricePlusTax(totalPrice));
+
+        Double[] numbers = {29.99,9.99,11.77};
+        Arrays.sort(numbers, Collections.reverseOrder());
+        for(double number : numbers){
+            System.out.println(number);
+        }
 
     }
 
@@ -28,9 +45,58 @@ public class MethodJava {
 
     }
 
-    static double luasKotak(double sisi){
+    private static double luasKotak(double sisi){
         return sisi*sisi;
     }
 
+    static boolean isLogin(String username, String password){
+        return username.equals("admin") && username.equals("p4ssw0rd");
+    }
+
+    static void displayDetailProduct(String productName, double price, int stock, int amount){
+        System.out.println(productName);
+        System.out.println(price);
+        System.out.println(stock);
+        System.out.println(amount);
+        // checkStock
+        checkStock(stock,amount);
+        // subTotal
+        System.out.println(subTotal(price,amount));
+
+    }
+
+    private static void checkStock(int stock, int amount){
+        // add condition
+        if(stock > 0){
+            if(amount <= stock) {
+                System.out.println("Stock tersedia "+stock+" pcs");
+                System.out.println("Button cart clickable");
+            } else {
+                System.out.println("Stock hanya tersedia "+stock+" pcs");
+                System.out.println("Button cart not clickable");
+            }
+        } else {
+            System.out.println("Stock kosong");
+            System.out.println("Button cart not clickable");
+        }
+
+    }
+
+    private static double subTotal(double price, int amount){
+        return price * amount;
+    }
+
+    // totalPrice
+    private static double totalPrice(double[] prices){
+        double sum = 0;
+        for (int i = 0; i < prices.length; i++) {
+            sum += prices[i];
+        }
+        return sum;
+    }
+
+    private static double totalPricePlusTax(double totalPrice){
+        return totalPrice+(totalPrice*11/100);
+    }
 
 }
